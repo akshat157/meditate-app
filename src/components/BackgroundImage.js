ea00f105a56725ae75fa0cc2b665988287969de1
ashuvssut <ashu.khanduala@gmail.com>
2021-10-20 02:44:29 +0530

    implemented modular css

--- src/components/BackgroundImage.js
+++ src/components/BackgroundImage.js
@@ -1,21 +1,37 @@
 import React from 'react'
+import styles from './../App.module.css'
 
 function BackgroundImage({ currentImage }) {
   const getClassName = (imgUrl) => {
-    let className = "bg "
-    if(currentImage === imgUrl){
-      className += "activeBg";
+    let className = styles.bg + ' '
+    if (currentImage === imgUrl) {
+      className += styles.activeBg
     }
-    return className;
+    return className
   }
-  
+
   return (
     <>
-      <div className={getClassName("img/rain.jpg")} style={{ backgroundImage: `url("img/rain.jpg")` }} />
-      <div className={getClassName("img/forest.jpg")} style={{ backgroundImage: `url(img/forest.jpg)` }} />
-      <div className={getClassName("img/park.jpg")} style={{ backgroundImage: `url(img/park.jpg)` }} />
-      <div className={getClassName("img/stream.jpg")} style={{ backgroundImage: `url(img/stream.jpg)` }} />
-      <div className={getClassName("img/waves.jpg")} style={{ backgroundImage: `url(img/waves.jpg)` }} />
+      <div
+        className={getClassName('img/rain.jpg')}
+        style={{ backgroundImage: `url("img/rain.jpg")` }}
+      />
+      <div
+        className={getClassName('img/forest.jpg')}
+        style={{ backgroundImage: `url(img/forest.jpg)` }}
+      />
+      <div
+        className={getClassName('img/park.jpg')}
+        style={{ backgroundImage: `url(img/park.jpg)` }}
+      />
+      <div
+        className={getClassName('img/stream.jpg')}
+        style={{ backgroundImage: `url(img/stream.jpg)` }}
+      />
+      <div
+        className={getClassName('img/waves.jpg')}
+        style={{ backgroundImage: `url(img/waves.jpg)` }}
+      />
     </>
   )
 }
