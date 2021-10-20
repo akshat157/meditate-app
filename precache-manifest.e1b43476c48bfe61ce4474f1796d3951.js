7edd9c3d8a6d64e00c0662253cca37ee9c4a3fa5
akshat157 <ancientfuture1337@gmail.com>
2021-10-20 16:56:12 +0530

    Updates

---
+++ precache-manifest.e1b43476c48bfe61ce4474f1796d3951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "decf1d6c0739fff5da98",
+    "url": "/meditate-app/static/css/main.91f0a6d2.chunk.css"
+  },
+  {
+    "revision": "decf1d6c0739fff5da98",
+    "url": "/meditate-app/static/js/main.76450bce.chunk.js"
+  },
+  {
+    "revision": "9e310ca9d7e515a8e7dc",
+    "url": "/meditate-app/static/js/runtime~main.a415c13b.js"
+  },
+  {
+    "revision": "d160861a4eee4bf3f451",
+    "url": "/meditate-app/static/css/2.d73eaaaf.chunk.css"
+  },
+  {
+    "revision": "d160861a4eee4bf3f451",
+    "url": "/meditate-app/static/js/2.a84145dc.chunk.js"
+  },
+  {
+    "revision": "0e4ea32681b82c96d6a2f2c0403f587f",
+    "url": "/meditate-app/index.html"
+  }
+];
